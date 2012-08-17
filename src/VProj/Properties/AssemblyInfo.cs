210b35482b7c1fd7441d805671421dd192cbf8a4
= <=>
2012-08-17 11:24:03 +1000

    Update AssemblyInfo version.

--- src/VProj/Properties/AssemblyInfo.cs
+++ src/VProj/Properties/AssemblyInfo.cs
@@ -1,14 +1,13 @@
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Change VS Project Version")]
 [assembly: AssemblyCopyright("Copyright © John Jeffery 2012")]
 [assembly: AssemblyCulture("")]
-
 [assembly: ComVisible(false)]
 [assembly: Guid("61aa2e18-d8d0-402d-8a79-bf73570d396a")]
 
 // Version is modified during the build by TeamCity
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
